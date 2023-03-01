3fdd9ceb574b514990fbd1086d0fa288caf80a21
kentfx1 <105797352+kentfx1@users.noreply.github.com>
2023-03-01 11:52:07 -0800

    my last commit for the day

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var header = document.querySelector('nav');
 window.onscroll = function(){
     var top = window.scrollY;
     console.log(top);
-    if (top >= 100) {
+    if (top >= 50) {
         header.classList.add("scrolled");
     }else{
         header.classList.remove("scrolled");
